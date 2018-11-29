fc5c97f017c676933f81edbce9165b01aef3dc9f
nlhuy <nguyen.luong.huy@framgia.com>
2018-11-29 14:09:19 +0700

    Finish Chapter 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    root "static_pages#home"
+
+    get "static_pages/about"
+    get "static_pages/contact"
+    get "static_pages/help"
+    get "static_pages/home"
+  end
 end
