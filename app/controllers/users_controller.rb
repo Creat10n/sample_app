faa5bc32c4cd7f4807696ea471b73818743c13c2
nlhuy <nguyen.luong.huy@framgia.com>
2018-12-11 15:26:02 +0700

    Finish Chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
-  def show
-    redirect_to signup_path unless @user = User.find_by(id: params[:id])
+  before_action :load_user, except: [:index, :new, :create]
+  before_action :logged_in_user, except: [:show, :new, :create]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate page: params[:page],
+      per_page: Settings.users.paginate.per_page
   end
 
   def new
@@ -11,16 +17,75 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       log_in @user
-      flash[:success] = t("users.new.sign_up.success")
+      flash[:success] = t "users.create.success"
       redirect_to @user # equivalent: redirect_to user_url(@user)
     else
+      flash.now[:danger] = t "users.create.fail"
       render :new
     end
   end
 
+  def show; end
+
+  def edit; end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = t "users.update.success"
+      redirect_to @user
+    else
+      flash[:danger] = t "users.update.fail"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "users.destroy.success"
+    else
+      flash[:danger] = t "users.destroy.fail"
+    end
+    redirect_to users_path
+  end
+
   private
   def user_params
-    params.require(:user).permit(:name, :email,
-      :password, :password_confirmation)
+    params.require(:user).permit :name, :email,
+      :password, :password_confirmation
+  end
+
+  # Load user from database using id
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:danger] = t "users.flash.not_found"
+    redirect_to login_path
+  end
+
+  # Before filters
+  # Confirms a logged-in user.
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t "users.flash.not_logged_in"
+    redirect_to login_path
+  end
+
+  # Confirm the correct user.
+  def correct_user
+    return if current_user? @user
+
+    flash[:danger] = t "users.flash.not_correct_user"
+    redirect_to root_path
+  end
+
+  # Confirms an admin user.
+  def admin_user
+    return if current_user.admin?
+
+    flash[:danger] = t "user.flash.not_admin"
+    redirect_to root_path
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,10 +7,14 @@ module SessionsHelper
   def check_log_in user, params
     if user && user.authenticate(params[:session][:password])
       log_in user
-      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      redirect_to user
+      if params[:session][:remember_me] == Settings.sessions.remember_me
+        remember user
+      else
+        forget user
+      end
+      redirect_back_or user
     else
-      flash.now[:danger] = t("sessions.new.login.fail")
+      flash.now[:danger] = t "sessions.new.login.fail"
       render :new
     end
   end
@@ -27,12 +31,17 @@ module SessionsHelper
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
+      user = User.find_by id: user_id
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
       end
     end
+  end
+
+  # Returns true if the given user is the current user.
+  def current_user? user
+    user == current_user
   end
 
   # Returns true if the user is logged in, false otherwise.
@@ -43,14 +52,25 @@ module SessionsHelper
   # Forgets a persistent session.
   def forget user
     user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
+    cookies.delete :user_id
+    cookies.delete :remember_token
   end
 
   # Logs out the current user.
   def log_out
-    forget(current_user)
-    session.delete(:user_id)
+    forget current_user
+    session.delete :user_id
     @current_user = nil
+  end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or default
+    redirect_to(session[:forwarding_url] || default)
+    session.delete :forwarding_url
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
